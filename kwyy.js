3b93bfaa7087e6826b78cdc3fe62c54b768c2149
LI MM <105048516+wwg135@users.noreply.github.com>
2022-05-07 11:39:20 +0800

    Update kwyy.js

--- kwyy.js
+++ kwyy.js
@@ -1,12 +1,7 @@
 /*
 應用名稱：KWYY CRACK
-軟件版本：3.0.5
-下載地址：https://apps.apple.com/cn/app/id588673713
-腳本作者：Cuttlefish
-微信賬號：公眾號墨魚手記
-更新時間：2022-02-12
-通知頻道：https://t.me/ddgksf2021
-問題反饋：https://t.me/ddgksf2013_bot
+軟件版本：3.1.0
+更新時間：2022-05-07
 [rewrite_local]
 # ～ KWYY_酷我聽書Block_ad@ddgksf2013
 https?:\/\/audiobookpay\.kuwo\.cn/a\.p\?op=get_advertright url reject-dict
